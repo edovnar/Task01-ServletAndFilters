7d748aa2ade6127ce66ae4dfd1b2f31ea54cc58c
Elena Dovnar <dr.dovn@gmail.com>
2020-08-19 15:37:12 +0300

    misprint

--- src/main/java/ioc/utils/JsonUtil.java
+++ src/main/java/ioc/utils/JsonUtil.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JsonUtil {
-    public ObjectMapper objectMapper;
+    private ObjectMapper objectMapper;
 
     private JsonUtil(){
         this.objectMapper = new ObjectMapper();
